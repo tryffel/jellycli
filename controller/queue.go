5d57e31f21a5f79d5fce241b1a9056ea87605377
Tryffel <tero.vierimaa@iki.fi>
2019-10-23 16:42:29 +0300

    Implement queue & history

---
+++ controller/queue.go
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2019 Tero Vierimaa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controller
+
+import (
+	"tryffel.net/pkg/jellycli/models"
+)
+
+type queue struct {
+	items    []*models.Song
+	history  []*models.Song
+	updateCb func([]*models.Song)
+}
+
+func (q *queue) GetQueue() []*models.Song {
+	return q.items
+}
+
+func (q *queue) ClearQueue() {
+	q.items = []*models.Song{}
+	q.notifyUpdates()
+}
+
+func (q *queue) QueueDuration() int {
+	duration := 0
+	for _, v := range q.items {
+		duration += v.Length
+	}
+	return duration
+}
+
+func (q *queue) AddSongs(songs []*models.Song) {
+	q.items = append(q.items, songs...)
+	q.notifyUpdates()
+}
+
+func (q *queue) Reorder(currentIndex, newIndex int) {
+	//TODO: Fix ordering songs
+	if currentIndex < 0 || newIndex < 0 {
+		return
+	}
+	last := len(q.items) - 1
+	if currentIndex > last || newIndex > last {
+		return
+	}
+
+	song := q.items[currentIndex]
+
+	var temp []*models.Song
+	if currentIndex < newIndex {
+		temp = append(q.items[:currentIndex], q.items[currentIndex+1:newIndex+1]...)
+		temp = append(temp, song)
+		temp = append(temp, q.items[newIndex+1:]...)
+	} else {
+		if newIndex == 0 {
+			temp = []*models.Song{song}
+		} else {
+			temp = append(q.items[:newIndex], song)
+		}
+		temp = append(temp, q.items[newIndex:currentIndex]...)
+	}
+
+	q.items = temp
+	q.notifyUpdates()
+}
+
+func (q *queue) GetHistory(n int) []*models.Song {
+	if n > len(q.history) {
+		return q.history
+	}
+	return q.history[:n]
+}
+
+func (q *queue) SetQueueChangedCallback(cb func(content []*models.Song)) {
+	q.updateCb = cb
+}
+
+func (q *queue) RemoveQueueChangedCallback() {
+	q.updateCb = nil
+}
+
+func (q *queue) notifyUpdates() {
+	if q.updateCb == nil {
+		return
+	}
+	q.updateCb(q.items)
+}
+
+func (q *queue) songComplete() {
+	if len(q.items) == 0 {
+		return
+	}
+	song := q.items[0]
+	q.items = q.items[1:]
+	if q.history == nil {
+		q.history = []*models.Song{song}
+	} else {
+		q.history = append([]*models.Song{song}, q.history...)
+	}
+}
---
+++ controller/queue_test.go
@@ -0,0 +1,251 @@
+/*
+ * Copyright 2019 Tero Vierimaa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controller
+
+import (
+	"reflect"
+	"testing"
+	"tryffel.net/pkg/jellycli/models"
+)
+
+func testSongs() []*models.Song {
+	return []*models.Song{
+		{Id: "song-1", Name: "song-1", Length: 60},
+		{Id: "song-2", Name: "song-2", Length: 10},
+		{Id: "song-3", Name: "song-3", Length: 1},
+		{Id: "song-4", Name: "song-4", Length: 350},
+		{Id: "song-5", Name: "song-5", Length: 10},
+		{Id: "song-6", Name: "song-6", Length: 10},
+		{Id: "song-7", Name: "song-7", Length: 10},
+		{Id: "song-8", Name: "song-8", Length: 80},
+		{Id: "song-9", Name: "song-9", Length: 80},
+	}
+}
+
+func Test_queue_GetQueue(t *testing.T) {
+	tests := []struct {
+		name  string
+		songs []*models.Song
+	}{
+		{
+			songs: []*models.Song{
+				{
+					Id:   "song-a",
+					Name: "song-a",
+				},
+				{
+					Id:   "song-b",
+					Name: "song-b",
+				},
+				{
+					Id:   "song-c",
+					Name: "song-c",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := &queue{
+				items: tt.songs,
+			}
+			if got := q.GetQueue(); !reflect.DeepEqual(got, tt.songs) {
+				t.Errorf("GetQueue() = %v, want %v", got, tt.songs)
+			}
+		})
+	}
+}
+
+func Test_queue_Reorder(t *testing.T) {
+	songs := testSongs()
+
+	type ordering struct {
+		from int
+		to   int
+	}
+
+	tests := []struct {
+		name      string
+		songs     []*models.Song
+		want      []*models.Song
+		orderings []ordering
+	}{
+		{
+			name:  "first-to-last",
+			songs: songs,
+			want: []*models.Song{
+				songs[1], songs[2], songs[3], songs[4], songs[5], songs[6],
+				songs[7], songs[8], songs[0],
+			},
+			orderings: []ordering{
+				{0, 8},
+			},
+		},
+		{
+			name:  "last-to-first",
+			songs: songs,
+			want: []*models.Song{
+				songs[8], songs[0], songs[1], songs[2], songs[3], songs[4],
+				songs[5], songs[6], songs[7],
+			},
+			orderings: []ordering{
+				{8, 0},
+			},
+		},
+		{
+			name:  "first-to-n, n-to-first",
+			songs: songs,
+			want: []*models.Song{
+				songs[5], songs[1], songs[2], songs[3], songs[4],
+				songs[0], songs[6], songs[7], songs[8],
+			},
+			orderings: []ordering{
+				{0, 5},
+				{4, 0},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := &queue{
+				items: make([]*models.Song, len(songs)),
+			}
+			copy(q.items, songs)
+			for _, v := range tt.orderings {
+				q.Reorder(v.from, v.to)
+			}
+			if got := q.GetQueue(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Reorder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_queue_songComplete(t *testing.T) {
+	songs := testSongs()
+	tests := []struct {
+		name     string
+		songs    []*models.Song
+		complete int
+		want     []*models.Song
+	}{
+		{
+			songs:    songs,
+			complete: 1,
+			want:     []*models.Song{songs[0]},
+		},
+		{
+			songs:    songs,
+			complete: 4,
+			want:     []*models.Song{songs[3], songs[2], songs[1], songs[0]},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := &queue{
+				items:   tt.songs,
+				history: []*models.Song{},
+			}
+			for i := 0; i < tt.complete; i++ {
+				q.songComplete()
+			}
+
+			history := q.GetHistory(10)
+			if !reflect.DeepEqual(history, tt.want) {
+				t.Errorf("TestQueue songComplete history: got %v, want: %v", history, tt.want)
+			}
+
+			if !reflect.DeepEqual(q.GetQueue(), tt.songs[tt.complete:]) {
+				t.Errorf("TestQueue songComplete remove items: got %v, want: %v",
+					q.GetQueue(), tt.songs[tt.complete])
+			}
+		})
+	}
+}
+
+func Test_queue_AddSongs(t *testing.T) {
+	songs := testSongs()
+	tests := []struct {
+		songs []*models.Song
+		name  string
+		add   []*models.Song
+		want  []*models.Song
+	}{
+		{
+			songs: songs,
+			add:   []*models.Song{songs[1], songs[2], songs[3]},
+			want:  append(songs, songs[1], songs[2], songs[3]),
+		},
+		{
+			songs: nil,
+			add:   []*models.Song{songs[1], songs[2], songs[3]},
+			want:  []*models.Song{songs[1], songs[2], songs[3]},
+		},
+		{
+			songs: songs,
+			add:   nil,
+			want:  songs,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := &queue{
+				items: tt.songs,
+			}
+			q.AddSongs(tt.add)
+			got := q.GetQueue()
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AddSongs, got: %v, want: %v", got, tt.want)
+			}
+
+		})
+	}
+}
+
+func Test_queue_QueueDuration(t *testing.T) {
+	songs := testSongs()
+
+	tests := []struct {
+		songs []*models.Song
+		name  string
+		want  int
+	}{
+		{
+			songs: songs,
+			want:  611,
+		},
+		{
+			songs: nil,
+			want:  0,
+		},
+		{
+			songs: []*models.Song{{Length: 1}},
+			want:  1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := &queue{
+				items: tt.songs,
+			}
+			if got := q.QueueDuration(); got != tt.want {
+				t.Errorf("QueueDuration() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
